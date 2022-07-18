128a47bf14a3ca355965b098f8e7abe34fa8f620
Rishi Kumar <rishi3pb2@gmail.com>
2022-07-18 11:33:35 +0530

    navbar component

---
+++ frontend/src/components/navbar/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className='container'>
+                <a className="navbar-brand" href="/">Send OTP Message</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Contacts</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/messages">Messages</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
